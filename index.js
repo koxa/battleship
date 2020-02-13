a9b9c1b515bfd9ad023dfc6efe1ef83b22f36010
cpriemski <cpriemski@intellisurvey.com>
2020-02-13 02:18:06 -0500

    board and ships generated

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
+const SHIP_HIT = Symbol();
+const SHIP_MISS = Symbol();
+const SHIP_ALREADY_ATTACKED = Symbol();
+const SHIP_SUNK = Symbol();
+
 class Battleship {
+    constructor(maxX, maxY, ships = [5, 4, 3, 2, 1]) {
+        this.board = new Board(maxX, maxY, ships);
+        console.log(this.board);
+        this.board.printBoard();
+    }
+
+    attack(x, y) { // convert 1:maxX to 0:maxX-1
+        x = x - 1;
+        y = y - 1;
+        const ship = this.board[x][y]; // get reference to ship if available
+        if (ship) {
+            const result = ship.hit(x, y); // hit ship's body
+            switch (result) {
+                case SHIP_HIT:
+                    console.log('SHIP HIT');
+                    break;
+                case SHIP_ALREADY_ATTACKED:
+                    console.log('SHIP ALREADY ATTACKED');
+                    break;
+                case SHIP_SUNK:
+                    console.log('SHIP SUNK');
+            }
+        } else {
+            console.log('SHIP MISS');
+        }
+    }
 
 }
 
 class Board {
-    constructor(x = 10, y = 10, ships = [5, 4, 3, 2, 1]) {
-        this.x = x;
-        this.y = y;
-        this.generateBoard(x, y);
-        this.generateShips(ships);
-        this.drawBoard();
-        //console.log('board', this.board);
+    constructor(maxX = 10, maxY = 10, shipSizes = [5, 4, 3, 2, 1]) {
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.generateBoard(maxX, maxY);
+        this.generateShips(shipSizes);
     }
 
-    drawBoard() {
-        for(let x = 0; x< this.board.length; x++ ) {
+    printBoard() {
+        for (let x = 0; x < this.board.length; x++) {
             console.log(this.board[x].join(' '));
         }
     }
@@ -28,53 +57,121 @@ class Board {
         }
     }
 
-    generateShips(ships) {
+    generateShips(shipSizes) {
         if (!this.board.length || !this.board[0].length) {
             throw new Error('There should be at least 1 cell in a board');
         }
-        for (let ship of ships) {
-            let shipCoords = this.generateShip(ship);
-            console.log(shipCoords);
-            this.putShip(shipCoords);
+        for (let shipLen of shipSizes) {
+            let ship = this.genShip(shipLen);
+            this.placeShip(ship);
         }
     }
 
-    putShip(shipCoords) {
-        for (let coord of shipCoords) {
-            let XY = coord.split(':');
-            this.board[XY[0]][XY[1]] = 1;
+    placeShip(ship) {
+        for (let coord of ship.getCoords()) {
+            this.board[coord.x][coord.y] = ship;
         }
     }
 
-    generateShip(shipLen, dir = Math.random() >= 0.5, prevPos = [], times = 0) {
-        times++;
-        const posX = Math.floor(Math.random() * this.x);
-        const posY = Math.floor(Math.random() * this.y);
-        if (prevPos.indexOf(posX + ':' + posY) > -1) {
-            return this.generateShip(shipLen, !dir, prevPos, times);
+    genShip(shipLen, retries = 0) {
+        //todo: check whether there is enough space left on board to fit new ship
+        const startX = Math.floor(Math.random() * this.maxX); // 0 to 9
+        const startY = Math.floor(Math.random() * this.maxY); // 0 to 9
+        const dir = Math.random() >= 0.5; // random direction; true = add X, false = add Y
+        const checkPos = dir ? startX : startY;
+        const checkMax = dir ? this.maxX : this.maxY;
+        // check if ship can fit into board
+        if (checkPos + shipLen > checkMax) {
+            return this.genShip(shipLen, ++retries);
+        } else {
+            // check if ship collides with other ships
+            const ship = new Ship(shipLen, startX, startY, dir);
+            if (this.isCollision(ship)) {
+                return this.genShip(shipLen, ++retries);
+            }
+            console.log('ship generated in ' + retries + ' retries');
+            return ship;
         }
-        let ship = [];
-        for (let i = 0; i < shipLen; i++) {
-            let x = dir ? posX + i : posX;
-            let y = dir ? posY : posY + 1;
-            console.log('trying x:' + x, ' y:' + y);
-            let val = this.board[x] ? this.board[x][y] : undefined;
-            if (val === undefined || val === 1) {
-                prevPos.push(posX + ':' + posY);
-                return this.generateShip(shipLen, !dir, prevPos, times);
-            } else {
-                ship.push(x + ':' + y);
+    }
+
+    isCollision(ship) {
+        for (let coord of ship.getCoords()) {
+            if (this.board[coord.x][coord.y] instanceof Ship) {
+                return true
             }
         }
-        // record ship on board
-        console.log('ship generated after ' + times + ' times');
-        return ship;
+        return false;
     }
-
 }
 
 class Ship {
+    constructor(len = 1, startX, startY, dir) {
+        this.len = len;
+        this.startX = startX;
+        this.startY = startY;
+        this.dir = dir;
+        this.uniqueHits = 0;
+        this.sunk = false;
+        this.body = [];
+        for (let i = 0; i < this.len; i++) {
+            this.body[i] = 1; // fill body to track hits, 1 means not hit
+        }
+    }
 
+    toString() {
+        return '' + this.len;
+    }
+
+    getCoords() {
+        const out = [];
+        for (let i = 0; i < this.len; i++) {
+            out.push({
+                x: this.dir ? this.startX + i : this.startX,
+                y: this.dir ? this.startY : this.startY + i
+            })
+        }
+        return out;
+    }
+
+    getXYIndex(x, y) {
+        const xMax = this.dir ? this.startX + this.len : this.startX;
+        const yMax = this.dir ? this.startY : this.startY + this.len;
+        if (x >= this.startX && x <= xMax) {
+            if (y >= this.startY && y <= yMax) {
+                return this.dir ? x - this.startX : y - this.startY;
+            }
+        }
+        return false;
+    }
+
+    hit(x, y) {
+        const hitIndex = this.getXYIndex(x, y);
+        if (!hitIndex) {
+            return SHIP_MISS;
+        }
+        const val = this.body[hitIndex];
+        if (val) {
+            if (val === 1) {
+                // hit
+                this.uniqueHits++;
+                this.body[hitIndex] = 2; // mark as hit
+                if (this.uniqueHits === this.len) {
+                    this.sunk = true;
+                    return SHIP_SUNK;
+                }
+                return SHIP_HIT;
+            } else if (val === 2) {
+                // already attacked
+                return SHIP_ALREADY_ATTACKED
+            }
+        } else {
+            // missed
+            return SHIP_MISS;
+        }
+    }
 }
 
-new Board();
+
+const BS = new Battleship(5, 5);
+console.log('row 1:', BS.board[1]);
+BS.attack(1, 1);
